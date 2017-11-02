2ca12c7d043642e3d2b3dfbed59505d6592bfc53
Andrew Ward <andrew.rampant@gmail.com>
2017-11-01 20:16:33 -0500

    first commit in personal branch

--- playground/map/mapapp.js
+++ playground/map/mapapp.js
@@ -1,16 +1,40 @@
 function initMap() {
     var uluru = {lat: -25.363, lng: 131.044};
+    var edwardsCampus = {lat: 38.899146, lng: -94.724889};
+    var kansasCityMo = {lat: 39.0997, lng: -94.5786};
+    var denverCo = {lat: 39.761849, lng: -104.880625};
+    var sanFranciscoCa = {lat: 37.727239, lng: -123.032229};
+    var newYorkNy = {lat: 40.664274, lng: -73.9385};
+    var austinTx = {lat: 30.307182, lng: -97.755996};
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
+      zoom: 5,
+      center: edwardsCampus
     });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
+    var marker0 = new google.maps.Marker({
+	  position: edwardsCampus,
+	  map: map
+	});
+	var marker1 = new google.maps.Marker({
+	  position: kansasCityMo,
+	  map: map
+	});
+	var marker2 = new google.maps.Marker({
+	  position: denverCo,
+	  map: map
+	});
+	var marker3 = new google.maps.Marker({
+	  position: sanFranciscoCa,
+	  map: map
+	});
+	var marker4 = new google.maps.Marker({
+	  position: newYorkNy,
+	  map: map
+	});
+	var marker5 = new google.maps.Marker({
+	  position: austinTx,
+	  map: map
+	});
 }
-
-//src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap
 
 
 
