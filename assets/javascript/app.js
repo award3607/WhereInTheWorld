9d48952c14c5eebe0bec6d6708e8059c48dc0c82
Andrew Ward <andrew.rampant@gmail.com>
2017-11-10 11:09:40 -0600

    Figured out .when() stuff for AJAX calls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,27 @@
-var hotelUrl = "http://api.hotwire.com/v1/search/hotel";
+//global constants
+const hotelUrl = "http://api.hotwire.com/v1/search/hotel";
+const carUrl = "http://api.hotwire.com/v1/search/car";
+const firebaseConfig = {
+	apiKey: "AIzaSyAis8sLxqMEv00Zmy1h00LwK3GfXul3i0g",
+	authDomain: "whereintheworld-1509580469553.firebaseapp.com",
+	databaseURL: "https://whereintheworld-1509580469553.firebaseio.com",
+	projectId: "whereintheworld-1509580469553",
+	storageBucket: "",
+	messagingSenderId: "601646874065"
+};
+const hotwireKey = "4hjyj79mhvxkvswbx5mur2xp";
+
+//global variables
+var destsArr = [];
 
 var hotelOptions = {
-	"dest": "Atlanta",
+	"dest": "Daytona Beach",
 	"startdate": "11/24/2017",
 	"enddate": "11/27/2017",
 	"rooms": "1",
 	"adults": "1",
 	"children": "0"
 };
-
-var carUrl = "http://api.hotwire.com/v1/search/car";
 
 var carOptions = {
 	"dest":"Atlanta",
@@ -19,45 +31,18 @@ var carOptions = {
 	"dropofftime":"13:30"
 };
 
-var airUrl = "http://api.hotwire.com/v1/tripstart/air";
+//firebase.initializeApp(firebaseConfig);
+//var ref = firebase.database().ref();
+var ref;
+var map;
+var markerArr = [];
 
-var airOptions = {
-		// "distance": "300",
-	"startdate": "10/15/2017",
-	"enddate": "11/1/2017",
-	"origin": "Kansas City",
-	"dest": "Atlanta"
-	// "price": "1000"
-}
-
-function generateSearchUrl(baseUrl, options) {
-
-	var url = baseUrl + "?apikey=4hjyj79mhvxkvswbx5mur2xp&format=jsonp";
-	Object.keys(options).forEach(function(key) {
-		url += "&" + key + "=" + options[key];
-	});
-	return url;
-}	
-
-function requestData(apiUrl, options) {
-	var searchUrl = generateSearchUrl(apiUrl, options);
-	console.log(searchUrl);
-
-	$.ajax({
-			url: searchUrl,
-			method: "GET",
-			dataType: 'jsonp',
-   			crossDomain: true
-		})
-		.done(function(response) {
-
-			console.log(response.Result);
-            var result = response.Result[0];
-            console.log(result.TotalPrice, result.DeepLink);
-            var $h = $("<div>").html("<a href=" + result.DeepLink + ">" + result.TotalPrice +"</a>");
-            $("body").append($h);
-		});
-}
+//listeners section
+$(document).ready(function() {
+	firebase.initializeApp(firebaseConfig);
+	ref = firebase.database().ref();
+	initMap();
+});
 
 $("#submit").on("click", function() {
 	event.preventDefault();
@@ -65,11 +50,42 @@ $("#submit").on("click", function() {
 	var tripLength = $("#trip-length").val().trim();
 	var numAdults = parseInt($("#trip-adults").val().trim());
 	var numChildren = parseInt($("#trip-children").val().trim());
-	console.log(destType, tripLength, numAdults, numChildren);
-	requestData(hotelUrl, hotelOptions);
-	requestData(carUrl, carOptions);
-	requestData(airUrl, airOptions);
-	displayResults(destType);
+	//console.log(destType, tripLength, numAdults, numChildren);
+
+	var promiseDests = displayResults(destType);
+	$.when(promiseDests).done(function() {
+		console.log("Firebase calls complete");
+		console.log(destsArr);
+		destsArr.forEach(function(dest) {
+			var promiseHotel = requestData(hotelUrl, hotelOptions);
+			var promiseCar = requestData(carUrl, carOptions);
+			$.when(promiseHotel, promiseCar).done(function() {
+				console.log("Hotel response? ");
+				var hotelResult = promiseHotel.responseJSON.Result[0];
+				console.log(hotelResult);
+				console.log("Car response? ");
+				var carResult = promiseCar.responseJSON.Result[0]
+				console.log(carResult);
+				console.log("Hotwire API calls complete");
+				//displayResultRow needs to go in here.
+				displayResultRow(dest, hotelResult, carResult);
+			});
+		});
+	});
+	
+	//TODO: build hotelOptions object
+	//TODO: build carOptions object
+
+	//get array of destinations
+	//for each destination
+	//we build the hotelOption object
+	//we build the carOptions object
+
+	// var promiseHotel = requestData(hotelUrl, hotelOptions);
+	// var promiseCar = requestData(carUrl, carOptions);
+	// $.when(promiseHotel, promiseCar).done(function() {
+	// 	console.log("Hotwire API calls complete");
+	// });
 });
 
 $("#reset").on("click", function() {
@@ -79,72 +95,58 @@ $("#reset").on("click", function() {
 	$("#trip-adults").val("");
 	$("#trip-children").val("");
 	clearMarkers();
+	destsArr = [];
+	$("tbody").empty();
 });
 
-var config = {
-	apiKey: "AIzaSyAis8sLxqMEv00Zmy1h00LwK3GfXul3i0g",
-	authDomain: "whereintheworld-1509580469553.firebaseapp.com",
-	databaseURL: "https://whereintheworld-1509580469553.firebaseio.com",
-	projectId: "whereintheworld-1509580469553",
-	storageBucket: "",
-	messagingSenderId: "601646874065"
-};
 
-firebase.initializeApp(config);
-var ref = firebase.database().ref();
-var map;
+//Hotwire API section
+function generateSearchUrl(baseUrl, options) {
 
+	var url = baseUrl + "?apikey=" + hotwireKey + "&format=jsonp";
+	Object.keys(options).forEach(function(key) {
+		url += "&" + key + "=" + options[key];
+	});
+	return url;
+}	
+
+function requestData(apiUrl, options) {
+	var searchUrl = generateSearchUrl(apiUrl, options);
+	//console.log(searchUrl);
+
+	return $.ajax({
+			url: searchUrl,
+			method: "GET",
+			dataType: 'jsonp',
+   			crossDomain: true
+		})
+		.done(function(response) {
+
+			// console.log(response.Result);
+            var result = response.Result[0];
+            // console.log(result.TotalPrice, result.DeepLink);
+            // var $h = $("<div>").html("<a href=" + result.DeepLink + ">" + result.TotalPrice +"</a>");
+            // $("#results").append($h);
+            // displayResultRow(destsArr[0], result.TotalPrice, result.DeepLink);
+		});
+}
+
+function displayResultRow(destination, hotelData, carData) {
+	var $row = $("<tr>");
+    $row.append($("<td>").text(destination.destCity))
+        .append($("<td>").html("<a href='" + hotelData.DeepLink + "' target='blank'>" + hotelData.TotalPrice + "</a>"))
+        .append($("<td>").html("<a href='" + carData.DeepLink + "' target='blank'>" + carData.TotalPrice + "</a>"));
+    $("tbody").append($row);
+}
+
+
+//Google Maps section
 function initMap() {
 	map = new google.maps.Map(document.getElementById("map"), {
 		zoom: 4,
 		center: {lat: 39.0997, lng: -94.5786}
 	});
 }
-
-function displayResults(dest) {
-    console.log("Hopefully: " + dest);
-	ref.orderByChild("destType").equalTo(dest).once("value", processResults, gotErr);
-}
-
-function processResults(data) {
-	var markers = data.val();
-    // console.log(markers);
-    //create array of keys
-	var keys = Object.keys(markers);
-    keys = shuffle(keys);
-    console.log(keys);
-	// console.log(markers);
-	keys.forEach(function(key) {
-		// console.log(markers[key]);
-		displayMarker(markers[key]);
-	});
-}
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-
-function gotErr(err) {
-	console.log("Error: " + err);
-}
-
-var markerArr = [];
 
 function displayMarker(destination) {
 	var marker = new google.maps.Marker({
@@ -169,12 +171,52 @@ function clearMarkers() {
 	markerArr = [];
 }
 
-$(document).ready(function() {
-	initMap();
-});
+
+//Firebase section
+function displayResults(dest) {
+    //console.log("Hopefully: " + dest);
+	return ref.orderByChild("destType").equalTo(dest).once("value", processResults, gotErr);
+}
+
+function processResults(data) {
+	var markers = data.val();
+    // console.log(markers);
+    //create array of keys
+	var keys = Object.keys(markers);
+    keys = shuffle(keys);
+    console.log(keys);
+	// console.log(markers);
+	keys.forEach(function(key) {
+		// console.log(markers[key]);
+		destsArr.push(markers[key]);
+		displayMarker(markers[key]);
+	});
+}
 
 
+//Utility functions section
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function gotErr(err) {
+	console.log("Error: " + err);
+}
 
 
 
